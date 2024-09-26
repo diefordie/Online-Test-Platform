047c685914dc193f676a4973fd9d66dd435122d1
alcynx <nabilachaa14@gmail.com>
2024-09-26 22:49:43 +0800

    connect fe be

--- frontend/src/app/cpns/page.jsx
+++ frontend/src/app/cpns/page.jsx
@@ -1,67 +1,78 @@
-"use client"; 
-import Link from 'next/link';
-import { useState } from 'react';
+'use client';
 
-const testData = [
-  {
-    id: 1,
-    kategori : "Try Out CPNS",
-    judul : "TRY OUT CPNS 2025#1",
-    prediksi_kemiripan: "Prediksi kemiripan 45%",
-    views: 1386,
-    author: " Rania Suyati",
-    free: true,
-    imageUrl: "/images/tes.png",
-    authorProfile : " /images/authorProfile.png"
-  },
-  {
-    id: 2,
-    kategori : "Try Out CPNS",
-    judul: "TRY OUT CPNS 2025#2",
-    prediksi_kemiripan: "Prediksi kemiripan 50%",
-    views: 2000,
-    author: " Desti Nur Irawati",
-    free: false, // ubah ke false untuk tes menampilkan gambar
-    imageUrl: "/images/tes.png",
-    authorProfile : " /images/authorProfile.png"
-  },
-  {
-    id: 3,
-    kategori : "Try Out CPNS",
-    judul: "TRY OUT CPNS 2025#2",
-    prediksi_kemiripan: "Prediksi kemiripan 55%",
-    views: 2000,
-    author: "Zhang Yixing",
-    free: true, // ubah ke false untuk tes menampilkan gambar
-    imageUrl: "/images/tes.png",
-    authorProfile : " /images/authorProfile.png"
-  },
-  {
-    id: 4,
-    kategori : "Try Out UTBK",
-    judul: "TRY OUT UTBK 2025#2",
-    prediksi_kemiripan: "Prediksi kemiripan 70%",
-    views: 1994,
-    author: " Oh Sehun",
-    free: true, // ubah ke false untuk tes menampilkan gambar
-    imageUrl: "/images/tes.png",
-    authorProfile : " /images/authorProfile.png"
-  },
-  {
-    id: 5,
-    kategori : "Try Out Pemrograman",
-    judul: "TRY OUT PEMROGRAMAN 2025#2",
-    prediksi_kemiripan: "Prediksi kemiripan 70%",
-    views: 1994,
-    author: " Oh Sehun",
-    free: true, // ubah ke false untuk tes menampilkan gambar
-    imageUrl: "/images/tes.png",
-    authorProfile : " /images/authorProfile.png"
-  },
-];
+import Link from 'next/link';
+import  { useState, useEffect } from 'react';
 
 export default function CPNS() {
+  const [popularTestsByCategory, setPopularTestsByCategory] = useState([]);
+  const [freeTestsByCategory, setFreeTestsByCategory] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState (['']);
+  const [loading, setLoading] = useState([true]);
+  const [error, setError] = useState([null]);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchPopularTestsByCategory = async () => {
+      try {
+        const response = await fetch('http://localhost:2000/dashboard/popular-tests-by-category?category=CPNS');
+        if (!response.ok) {
+          throw new Error('Failed to fetch popular tests by category');
+        }
+        const data = await response.json();
+        setPopularTestsByCategory(data);
+      } catch (error) {
+        console.error('Error fetching popular tests by category:', error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPopularTestsByCategory();
+  }, []);
+
+  useEffect(() => {
+    const fetchFreeTestsByCategory = async () => {
+      try {
+        const response = await fetch('http://localhost:2000/dashboard/free-tests-by-category?category=CPNS');
+        if (!response.ok) {
+          throw new Error('Failed to fetch free tests by category');
+        }
+        const data = await response.json();
+        setFreeTestsByCategory(data);
+      } catch (error) {
+        console.error('Error fetching free tests by category:', error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFreeTestsByCategory();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery) return;
+
+    try {
+      const response = await fetch(`http://localhost:2000/dashboard/search-tests?title=${encodeURIComponent(searchQuery)}`);
+      if (!response.ok) {
+        throw new Error('Failed to search tests');
+      }
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.error('Error searching tests:', error);
+      setError(error.message);
+    }
+  };
+
+  if (loading && !error) {
+    return <div className="text-center mt-20">Loading...</div>;
+  }
+
   return (
     <>
       <header className="bg-deepBlue text-white p-4">
@@ -134,11 +145,14 @@ export default function CPNS() {
     <section className="bg-gradient-custom p-20">
        {/* Search Bar */}
        <div className="container mx-auto mt-4 ">
-            <form className="flex mx-auto items-center p-1 rounded-2xl bg-white max-w-[610px] font-poppins  ">
+            <form onSubmit={handleSearch} className="flex mx-auto items-center p-1 rounded-2xl bg-white max-w-[610px] font-poppins  ">
             <input 
               type="text" 
               placeholder="Cari Tes Soal" 
-              className="flex-grow p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-powderBlue font-poppins"/>
+              className="flex-grow p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-powderBlue font-poppins"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              />
                <button 
                 type="submit" 
                 className="ml-auto p-2 text-deepBlue font-bold rounded-2xl hover:bg-gray-200 font-poppins">
@@ -152,23 +166,21 @@ export default function CPNS() {
         </div>
     </section>
 
-    {/* Bagian Paling Populer */}
-      <section className="bg-putih p-5">
+    {searchResults.length > 0 && (
+        <section className="bg-putih p-5 text-bold">
         <div className="container  mx-auto mt-5 font-bold font-poppins text-deepBlue ">
-          Paling Populer
+          Hasil Pencarian
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-4 gap-4 mt-8">
-      {testData
-        .filter((test) => test.kategori === "Try Out CPNS")
-        .map((test) => (
-          <div key={test.id} className="bg-abumuda shadow-lg p-1 relative group">
+      {searchResults.map((test) => (
+          <div key={test.testId} className="bg-abumuda shadow-lg p-1 relative group">
             {/* Overlay background abu-abu yang muncul saat hover */}
             <div className="absolute inset-0 bg-gray-500 opacity-0 group-hover:opacity-40 transition-opacity duration-300 z-10"></div>
 
             <div className="flex justify-between items-center relative z-20 group-hover:blur-sm transition duration-300">
               <div className="flex items-center space-x-2 font-bold text-deepBlue">
                 <img src="/images/eye-icon.png" alt="Views" className="h-4 w-4" />
-                <span className="text-sm">{test.views}</span>
+                <span className="text-sm">{test.accessCount}</span>
               </div>
               <div className="flex items-center space-x-2">
                 <img src="/images/share-icon.png" alt="Share" className="h-3 w-3" />
@@ -177,31 +189,31 @@ export default function CPNS() {
             </div>
 
             <div className="flex justify-center mt-4 relative z-20 group-hover:blur-sm transition duration-300">
-              <Link href={`/tes/${test.id}`}>
-                <img src={test.imageUrl} alt={test.kategori} className="h-20 w-20 cursor-pointer" />
+              <Link href={`/tes/${test.testId}`}>
+                <img src="/images/tes.png" alt={test.category} className="h-20 w-20 cursor-pointer" />
               </Link>
             </div>
 
             <div className="flex justify-center mt-4 text-deepBlue relative z-20 group-hover:blur-sm transition duration-300">
-              <h3 className="text-center text-lg font-bold mt-2">{test.kategori}</h3>
+              <h3 className="text-center text-lg font-bold mt-2">{test.category}</h3>
             </div>
 
             <div className="bg-deepBlue text-white p-2 mt-4 relative z-20 group-hover:blur-sm transition duration-300">
               <div className="flex items-center space-x-2 justify-between">
-                <h3 className="text-left text-base font-bold mt-2">{test.judul}</h3>
+                <h3 className="text-left text-base font-bold mt-2">{test.title}</h3>
                 <img src="/images/fav-icon.png" alt="More" className="h-7/2" />
               </div>
 
-              <p className="text-left text-sm leading-relaxed">{test.prediksi_kemiripan}</p>
+              <p className="text-left text-sm leading-relaxed">Prediksi kemiripan {test.similarity}</p>
               <p className="text-xs leading-relaxed">Dibuat Oleh :</p>
 
               <div className="flex justify-between space-x-2 leading-relaxed mt-1">
-                <div className="flex text-left space-x-4">
-                  <img src={test.authorProfile} alt={test.kategori} className="h-5 w-5" />
-                  <span className="text-sm font-semibold">{test.author}</span>
-                </div>
+              <div className='flex text-left leading-relaxed space-x-4 '>
+                <img src={test.author.authorPhoto} alt={test.author.nama} className="h-5 w-5 leading-relaxed " />
+                <span className="text-sm font-semibold leading-relaxed ">{test.author.nama}</span>
+              </div>
                 <span className="text-sm font-semibold">
-                  {test.free ? 'Gratis' : <img src="/images/lock.png" alt="Berbayar" className="h-9/2 inline-block" />}
+                  {test.price ? <img src="/images/lock.png" alt="Berbayar" className="h-9/2 inline-block" /> : 'Gratis'}
                 </span>
               </div>
             </div>
@@ -216,22 +228,23 @@ export default function CPNS() {
         </div>
 
       </section>
-
-      {/* Bagian Gratis */}
-      <section className="bg-putih p-5">
-      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-4 gap-4 mt-8">
-      {testData
-        .filter((test) => test.kategori === "Try Out CPNS") // Filter hanya data dengan kategori "Tru Out Pemrograman"
-        .filter((test) => test.free) // Filter hanya data dengan free = true
-        .map((test) => (
-          <div key={test.id} className="bg-abumuda shadow-lg p-1 relative group">
+      )}
+    {/* Bagian Paling Populer */}
+      {popularTestsByCategory.length > 0 && (
+        <section className="bg-putih p-5">
+        <div className="container  mx-auto mt-5 font-bold font-poppins text-deepBlue ">
+          Paling Populer
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-4 gap-4 mt-8">
+      {popularTestsByCategory.map((test) => (
+          <div key={test.testId} className="bg-abumuda shadow-lg p-1 relative group">
             {/* Overlay background abu-abu yang muncul saat hover */}
             <div className="absolute inset-0 bg-gray-500 opacity-0 group-hover:opacity-40 transition-opacity duration-300 z-10"></div>
 
             <div className="flex justify-between items-center relative z-20 group-hover:blur-sm transition duration-300">
               <div className="flex items-center space-x-2 font-bold text-deepBlue">
                 <img src="/images/eye-icon.png" alt="Views" className="h-4 w-4" />
-                <span className="text-sm">{test.views}</span>
+                <span className="text-sm">{test.accessCount}</span>
               </div>
               <div className="flex items-center space-x-2">
                 <img src="/images/share-icon.png" alt="Share" className="h-3 w-3" />
@@ -240,45 +253,108 @@ export default function CPNS() {
             </div>
 
             <div className="flex justify-center mt-4 relative z-20 group-hover:blur-sm transition duration-300">
-              <Link href={`/tes/${test.id}`}>
-                <img src={test.imageUrl} alt={test.kategori} className="h-20 w-20 cursor-pointer" />
+              <Link href={`/tes/${test.testId}`}>
+                <img src="/images/tes.png" alt={test.category} className="h-20 w-20 cursor-pointer" />
               </Link>
             </div>
 
             <div className="flex justify-center mt-4 text-deepBlue relative z-20 group-hover:blur-sm transition duration-300">
-              <h3 className="text-center text-lg font-bold mt-2">{test.kategori}</h3>
+              <h3 className="text-center text-lg font-bold mt-2">{test.category}</h3>
             </div>
 
             <div className="bg-deepBlue text-white p-2 mt-4 relative z-20 group-hover:blur-sm transition duration-300">
               <div className="flex items-center space-x-2 justify-between">
-                <h3 className="text-left text-base font-bold mt-2">{test.judul}</h3>
+                <h3 className="text-left text-base font-bold mt-2">{test.title}</h3>
                 <img src="/images/fav-icon.png" alt="More" className="h-7/2" />
               </div>
 
-              <p className="text-left text-sm leading-relaxed">{test.prediksi_kemiripan}</p>
+              <p className="text-left text-sm leading-relaxed">Prediksi kemiripan {test.similarity}</p>
               <p className="text-xs leading-relaxed">Dibuat Oleh :</p>
 
               <div className="flex justify-between space-x-2 leading-relaxed mt-1">
-                <div className="flex text-left space-x-4">
-                  <img src={test.authorProfile} alt={test.kategori} className="h-5 w-5" />
-                  <span className="text-sm font-semibold">{test.author}</span>
-                </div>
+              <div className='flex text-left leading-relaxed space-x-4 '>
+                <img src={test.author.authorPhoto} alt={test.author.nama} className="h-5 w-5 leading-relaxed " />
+                <span className="text-sm font-semibold leading-relaxed ">{test.author.nama}</span>
+              </div>
                 <span className="text-sm font-semibold">
-                  {test.free ? 'Gratis' : <img src="/images/lock.png" alt="Berbayar" className="h-9/2 inline-block" />}
+                  {test.price ? <img src="/images/lock.png" alt="Berbayar" className="h-9/2 inline-block" /> : 'Gratis'}
                 </span>
               </div>
             </div>
 
             {/* Tombol yang berada di bagian paling bawah */}
             <div className="absolute bottom-5 left-0 right-0 flex justify-center space-x-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-30 p-2">
-            <a href="/tes" className="bg-paleBlue text-deepBlue text-bold px-7 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Mulai</a>
-            <a href="/topScore" className="bg-paleBlue text-deepBlue text-bold px-4 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Top Score</a>
-          </div>
-
+              <a href="/tes" className="bg-paleBlue text-deepBlue text-bold px-7 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Mulai</a>
+              <a href="/topScore" className="bg-paleBlue text-deepBlue text-bold px-4 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Top Score</a>
+              </div>
           </div>
     ))}
         </div>
+
       </section>
+      )}
+
+      {/* Bagian Gratis */}
+      {freeTestsByCategory.length > 0 && (
+        <section className="bg-putih p-5">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-4 gap-4 mt-8">
+        {freeTestsByCategory.map((test) => (
+            <div key={test.testId} className="bg-abumuda shadow-lg p-1 relative group">
+              {/* Overlay background abu-abu yang muncul saat hover */}
+              <div className="absolute inset-0 bg-gray-500 opacity-0 group-hover:opacity-40 transition-opacity duration-300 z-10"></div>
+  
+              <div className="flex justify-between items-center relative z-20 group-hover:blur-sm transition duration-300">
+                <div className="flex items-center space-x-2 font-bold text-deepBlue">
+                  <img src="/images/eye-icon.png" alt="Views" className="h-4 w-4" />
+                  <span className="text-sm">{test.accessCount}</span>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <img src="/images/share-icon.png" alt="Share" className="h-3 w-3" />
+                  <img src="/images/more-icon.png" alt="More" className="h-7/2" />
+                </div>
+              </div>
+  
+              <div className="flex justify-center mt-4 relative z-20 group-hover:blur-sm transition duration-300">
+                <Link href={`/tes/${test.testId}`}>
+                  <img src="/images/tes.png" alt={test.category} className="h-20 w-20 cursor-pointer" />
+                </Link>
+              </div>
+  
+              <div className="flex justify-center mt-4 text-deepBlue relative z-20 group-hover:blur-sm transition duration-300">
+                <h3 className="text-center text-lg font-bold mt-2">{test.category}</h3>
+              </div>
+  
+              <div className="bg-deepBlue text-white p-2 mt-4 relative z-20 group-hover:blur-sm transition duration-300">
+                <div className="flex items-center space-x-2 justify-between">
+                  <h3 className="text-left text-base font-bold mt-2">{test.title}</h3>
+                  <img src="/images/fav-icon.png" alt="More" className="h-7/2" />
+                </div>
+  
+                <p className="text-left text-sm leading-relaxed">Prediksi kemiripan {test.similarity}</p>
+                <p className="text-xs leading-relaxed">Dibuat Oleh :</p>
+  
+                <div className="flex justify-between space-x-2 leading-relaxed mt-1">
+                <div className="flex text-left space-x-4">
+                  <img src={test.author.authorPhoto} alt={test.author.nama} className="h-5 w-5" />
+                  <span className="text-sm font-semibold">{test.author}</span>
+                </div>
+                  <span className="text-sm font-semibold">
+                  {test.price ? <img src="/images/lock.png" alt="Berbayar" className="h-9/2 inline-block" /> : 'Gratis'}
+                  </span>
+                </div>
+              </div>
+  
+              {/* Tombol yang berada di bagian paling bawah */}
+              <div className="absolute bottom-5 left-0 right-0 flex justify-center space-x-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-30 p-2">
+              <a href="/tes" className="bg-paleBlue text-deepBlue text-bold px-7 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Mulai</a>
+              <a href="/topScore" className="bg-paleBlue text-deepBlue text-bold px-4 py-2 rounded-full inline-block hover:bg-orange hover:text-deepBlue">Top Score</a>
+            </div>
+  
+            </div>
+      ))}
+          </div>
+        </section>
+      )}
        
     </>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,20 +8,29 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        poppins: ['Poppins'],
-      },
       colors: {
-        primary: "#5684AB",          // Warna utama
-        secondary: "#62A4D1",        // Warna sekunder
-        'primary-dark': '#0275a7',   // Warna primary lebih gelap
-        green: '#C5E4FF',   
-        putih: '#FFF7F7',
-        birutua: '#0E3876',// Warna hijau tambahan
-        birumuda: '#78AED6',
-        birulangit: '#77BEE1',
-        hitam: "#000000",
-        abu: "#6B7280",
+        primary: "#3887a7",    
+        primary2: "#06549D",   
+        secondary: "#c3e8e7",   
+        secondary2: "#9CCEFC",
+        'primary-dark': '#0275a7',   
+        green: '#92e2a8',  
+        biru: '#7BB3B4',  
+        birumuda: '#CFE9E6', 
+        putih: '#ffff', // ubah punya sipa jadi putih
+        abumuda: '#F3F3F3', 
+        deepBlue: '#0B61AA', 
+        orange :'#F8B75B',
+        powderBlue : '#78AED6',
+        paleBlue : '#CAE6F9',  
+        'blue-light': '#CFE9E6',
+        'login-bg': '#f4f7fc',
+        'form-gray': '#e2e8f0',
+        'button-hover': '#004e92',
+        'custom-gray': '#D9D9D9',    // Warna abu-abu custom
+        customBlue: '#0B61AA',       // Warna biru custom yang diperbarui
+        newHeaderColor: '#77BEE1',
+
       },
       container: {
         center: true,
@@ -33,7 +42,41 @@ module.exports = {
           "2xl": "6rem",
         },
       },
+      backgroundImage: {
+        'gradient-custom': 'linear-gradient(to right, #DEF6FF, #0B61AA)',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        bodoni: ['"Libre Bodoni"', 'serif'],
+      },
+      boxShadow: {
+        'customShadow': '0 40px 40px rgba(0, 0, 0, 0.25)',
+      },
+      spacing: {
+        '69': '17.25rem',
+        '71': '17.75rem',
+      },
+      fontSize: {
+        '2xl': '1.5rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+      },
+      fontFamily: {
+        appearance: ['Appearance', 'sans-serif', 'Poppins'], // Add Appearance font
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      width: {
+        '1/3': '33.3333%',
+        '2/3': '66.6667%',
+      },
+      minWidth: {
+        '320': '320px', // Example min-width
+      },
+
+     
+      
     },
   },
   plugins: [],
 };
+
