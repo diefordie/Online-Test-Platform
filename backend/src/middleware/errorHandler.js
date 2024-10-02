2842addb9be29049674b3d20811dc309c0805f7b
Gustavo <gusty.risandha@gmail.com>
2024-10-02 13:01:44 +0800

    tambah controller test

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,14 +1,10 @@
-// middleware/errorHandler.js
-
 const errorHandler = (err, req, res, next) => {
-    console.error(err); // Log error ke console
+    console.error(err); 
 
-    // Mengirim status dan pesan error berdasarkan jenis error
     if (err.status) {
         return res.status(err.status).json({ message: err.message });
     }
 
-    // Untuk error yang tidak terduga
     return res.status(500).json({ message: 'Internal Server Error' });
 };
 
